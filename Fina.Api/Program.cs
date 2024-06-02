82b28e4cb8334478def29ea246451366fc3aca21
Pedro Henrique <phlgasquez@gmail.com>
2024-06-02 17:30:26 -0300

    feat: db created / progam services

--- Fina.Api/Program.cs
+++ Fina.Api/Program.cs
@@ -1,4 +1,15 @@
+using Fina.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+const string connectionString =
+    "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;";
+
+builder.Services.AddDbContext<AppDbContext>(
+    x=>x.UseSqlServer(connectionString)
+    );
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
