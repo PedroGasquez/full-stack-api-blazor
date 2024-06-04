58f0d2269c24b5bfef9a2d22c5e8f21d94bffcd2
Pedro Henrique <phlgasquez@gmail.com>
2024-06-03 23:02:59 -0300

    feat: add services

--- Fina.Api/Program.cs
+++ Fina.Api/Program.cs
@@ -1,4 +1,6 @@
 using Fina.Api.Data;
+using Fina.Api.Handlers;
+using Fina.Core.Handlers;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +11,8 @@ const string connectionString =
 builder.Services.AddDbContext<AppDbContext>(
     x=>x.UseSqlServer(connectionString)
     );
-
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
