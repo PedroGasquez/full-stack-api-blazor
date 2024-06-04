ed253e21b2ca85d8c2cacdbd99902bf6c1e24a91
Pedro Henrique <phlgasquez@gmail.com>
2024-06-03 23:03:23 -0300

    fix: nullable contracts

--- Fina.Core/Handlers/ITransactionHandler.cs
+++ Fina.Core/Handlers/ITransactionHandler.cs
@@ -7,9 +7,9 @@ namespace Fina.Core.Handlers;
 
 public interface ITransactionHandler
 {
-    Task<Response<Transaction>> CreateAsync(CreateTransactionRequest request);
-    Task<Response<Transaction>> UpdateAsync(UpdateTransactionRequest request);
-    Task<Response<Transaction>> DeleteAsync(DeleteTransactionRequest request);
-    Task<Response<Transaction>> GetByIdAsync(GetCategoryByIdRequest request);
+    Task<Response<Transaction?>> CreateAsync(CreateTransactionRequest request);
+    Task<Response<Transaction?>> UpdateAsync(UpdateTransactionRequest request);
+    Task<Response<Transaction?>> DeleteAsync(DeleteTransactionRequest request);
+    Task<Response<Transaction?>> GetByIdAsync(GetCategoryByIdRequest request);
     Task<PagedResponse<List<Transaction>?>> GetByPeriodAsync(GetTransactionsByPeriodRequest request);
 }
